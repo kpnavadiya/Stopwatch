02d5de534053edbf76fd66f7cee3dbf32a0d3104
krunal <krunal.navadiya@solulab.co>
2019-01-26 01:59:52 +0530

    Add Local Storage Function to save History

--- main.js
+++ main.js
@@ -80,47 +80,53 @@ function resetTimer() {
     start.innerHTML = 'Start';
     printTime(houers = 0,minute = 0,second = 0,mili = 0);
    
+   // Remove Lap History from DOM
+    removeLap();
 }
 
 // Count Stopatch Lap
 function lapTimer() {
-        count();
+        //Save History in LocalStorage
+        saveInlocal(houers,minute,second,mili);
+
     const lapRow = document.createElement('tr');
     
     // Build a templet for lap
     lapRow.innerHTML = `
-            <tr>
-                
-            </tr>
-            <tr>
+           
+         <tr><td><a id="list"></a></td>
             <td> ${houers}: ${minute}: ${second}: ${mili} </td>
-            <td> ${lepTime.houers}: ${lepTime.minute}: ${lepTime.second}: ${lepTime.mili}</td>
-            </tr>
+               
+        </tr>
     `;
     // Add into Html
     
     lapListContent.appendChild(lapRow);    
 }
-let t=0;
+//let t=0;
 
-let lepTime = {
-    houers  , minute , second , mili };
-function count() {
-   
-    saveInlocal(second);
-}
+// let lepTime = {
+//     houers  , minute , second , mili };
 
 // Save into local storage
-function saveInlocal(second) {
+function saveInlocal(houers,minuet,second,mili) {
     
     let timeStore = getTimeFromStorage();
+    //let timeData;
+    if(houers!='00' || minuet!='00' || second != '00' || mili != '00'){
+        // Check the numbers of Lap
+        if(timeStore.length <= 10){
+            timeStore.shift();
+        }
+        let timeData = houers + ':' + minuet + ':' + second + ':' + mili;
     
-    //add into local storage
-    timeStore.push(second);
+         //add into local storage
+        timeStore.push(timeData);
     
-    // save as string into localStorage
-    localStorage.setItem('timeStore', JSON.stringify(timeStore));
-    
+         // save as string into localStorage
+        localStorage.setItem('timeStores', JSON.stringify(timeStore));
+    }
+       
 }
 
 // get time into storage
@@ -128,31 +134,43 @@ function getTimeFromStorage() {
     let timeStore;
     
     // Check if something exist on storage otherwise it create empty array
-    if(localStorage.getItem('timeStore') === null) {
+    if(localStorage.getItem('timeStores') === null) {
         timeStore = [];
     } else {
-        timeStore = JSON.parse(localStorage.getItem('timeStore'));
+        timeStore = JSON.parse(localStorage.getItem('timeStores'));
         
     }
     return timeStore;
 }
 
-// print the history of the lap
+// print the history of the lap from LocalStorage
 function show() {
     let timess = getTimeFromStorage();
-
+    console.log("Show History");
     //count all lap into loop
-    timess.forEach(function(second) {
+    timess.forEach(function(houers,minute,second,mili) {
         //create <tr> tage into html
         const row = document.createElement('tr');
 
         //print on html
         row.innerHTML = `
             <tr>
-                <td>${second.second} </td>
+                <td>${houers} : ${minute} : ${second} : ${mili}</td>
             </tr>
             `;
             showLapHistory.appendChild(row);
     });
+
+    // Remove from the localStorage After Showing History
+    localStorage.clear();
+}
+
+// Remove Lap
+function removeLap() {
+    // Remove All the Child of the laplist  
+    var list = document.getElementById('lapList');
+      while(list.hasChildNodes()) {
+          list.removeChild(list.firstChild);
+      }
     
 }
